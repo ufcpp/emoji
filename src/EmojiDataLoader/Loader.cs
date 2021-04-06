f71f491fcaef464377399e0e55be6cbe328b5d42
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-04-06 15:15:49 +0900

    Unicode 12.1 での生成も試してみる
    今のところとりあえず手作業でコメントアウト行を切り替える運用にしてる。

--- src/EmojiDataLoader/Loader.cs
+++ src/EmojiDataLoader/Loader.cs
@@ -10,6 +10,7 @@ namespace EmojiData
     {
         const string RepositoryName = "iamcal/emoji-data";
         const string BranchName = "56e5c532573edc8bdf8b16cb2e750e3cb383ad9d"; // 2021年3月。 Unicode 13.0。
+        //const string BranchName = "cff32eea1d876e4c4a73c87ebc2fa218775de58f"; // 2020年1月。 Unicode 12.1。
 
         //todo: master/main じゃなくて、特定のコミットから取らないとある日突然変わるので困るはず。
         // あと、違う URL (違うコミット)から取ったときはローカルストレージのキャッシュ無効化しないと。
