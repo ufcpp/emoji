d6934a4f707b6f98ed3dbdc19ea3f8887dcf77e1
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-04-08 13:29:53 +0900

    古い todo コメ削除 & using 整理

--- src/RgiSequenceFinder.TableGenerator/CategorizedEmoji.cs
+++ src/RgiSequenceFinder.TableGenerator/CategorizedEmoji.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RgiSequenceFinder.TableGenerator
 {
-    //todo: GroupedEmoji の置き換えなので動作確認取れたら GroupedEmoji の方消す。
-
     public struct CategorizedEmoji
     {
         /// <summary>
