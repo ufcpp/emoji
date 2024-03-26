74aff5196b60f5993133f5173f6daed99f2865d9
Nobuyuki Iwanaga <ufcpp@live.jp>
2024-03-26 13:58:53 +0900

    Unicode 15

--- src/EmojiDataLoader/Loader.cs
+++ src/EmojiDataLoader/Loader.cs
@@ -6,7 +6,8 @@ namespace EmojiData;
 public class Loader
 {
     const string RepositoryName = "iamcal/emoji-data";
-    const string BranchName = "1ddc9ca67c1379c372b4ca39824659f71caa2825"; // 2022年3月。 Unicode 14.0。
+    const string BranchName = "0669bda2b9348984eeb4ce66215819bf61c35811"; // 2023年5月。 Unicode 15.0。
+    //const string BranchName = "1ddc9ca67c1379c372b4ca39824659f71caa2825"; // 2022年3月。 Unicode 14.0。
     //const string BranchName = "675515762babff57a8d9c05f420806f1354203c3"; // 2022年1月。 Unicode 13.1。
     //const string BranchName = "56e5c532573edc8bdf8b16cb2e750e3cb383ad9d"; // 2021年3月。 Unicode 13.0。
     //const string BranchName = "cff32eea1d876e4c4a73c87ebc2fa218775de58f"; // 2020年1月。 Unicode 12.1。
